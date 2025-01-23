da6cd1e42b9c325f45588ae89dfd83288289a4c7
Leo <37211774+CaliforniaDev@users.noreply.github.com>
2025-01-23 01:21:55 -0800

    feat: implement form validation for registration using Zod and React Hook Form

--- auth-app/app/register/page.tsx
+++ auth-app/app/register/page.tsx
@@ -1,11 +1,32 @@
+'use client';
+
 import {
   Card,
   CardHeader,
   CardTitle,
   CardDescription,
 } from '@/components/ui/card';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useForm } from 'react-hook-form';
+import { z } from 'zod';
+
+const formSchema = z.object({
+  email: z.string().email(),
+  password: z.string().min(5, 'Password must be at least 5 characters'),
+  passwordConfirm: z.string(),
+});
+
+type FormData = z.infer<typeof formSchema>;
 
 export default function Register() {
+  const form = useForm<FormData>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      email: '',
+      password: '',
+      passwordConfirm: '',
+    },
+  });
   return (
     <main className='flex min-h-screen items-center justify-center'>
       <Card className='w-[350px]'>
