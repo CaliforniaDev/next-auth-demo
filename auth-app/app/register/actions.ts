96bbbc19970e906f59677cdd1f2bf05396bdb2a3
Leo <37211774+CaliforniaDev@users.noreply.github.com>
2025-01-29 06:02:07 -0800

    chore: add comments

--- auth-app/app/register/actions.ts
+++ auth-app/app/register/actions.ts
@@ -1,6 +1,8 @@
 'use server';
 
 import { z } from 'zod';
+import db from '@/db/drizzle';
+import { hash } from 'bcryptjs';
 import { passwordMatchSchema } from '@/validation/passwordMatchSchema';
 
 interface RegisterUser {
@@ -14,18 +16,20 @@ export const registerUser = async ({
   password,
   passwordConfirm,
 }: RegisterUser) => {
+  const result = await db.select();
+  // Check if the email already exists in the database
   const newUserScheme = z
     .object({
       email: z.string().email(),
     })
     .and(passwordMatchSchema);
-
+  // Validate the input data
   const newUserValidation = newUserScheme.safeParse({
     email,
     password,
     passwordConfirm,
   });
-
+  // If validation fails, return an error response
   if (!newUserValidation.success) {
     return {
       error: true,
@@ -33,4 +37,6 @@ export const registerUser = async ({
         newUserValidation.error.issues[0]?.message ?? 'An error occurred',
     };
   }
+
+
 };
