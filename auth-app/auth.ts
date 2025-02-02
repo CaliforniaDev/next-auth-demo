db46554b9a883acb8665ad4aa75a1f4703ccdbdb
Leo <37211774+CaliforniaDev@users.noreply.github.com>
2025-02-02 04:59:04 -0800

    feat: implement authentication handlers with NextAuth and middleware

---
+++ auth-app/auth.ts
@@ -0,0 +1,49 @@
+import NextAuth from 'next-auth';
+import Credentials from 'next-auth/providers/credentials';
+import db from './db/drizzle';
+import { users } from './db/usersSchema';
+import { eq } from 'drizzle-orm';
+import { compare } from 'bcryptjs';
+
+export const { handlers, signIn, signOut, auth } = NextAuth({
+  providers: [
+    Credentials({
+      credentials: {
+        email: {},
+        password: {},
+      },
+      authorize: async credentials => {
+        // Check if credentials are provided
+        if (!credentials) {
+          throw new Error('No credentials provided');
+        }
+
+        // Fetch the user from the database
+        const [user] = await db
+          .select()
+          .from(users)
+          .where(eq(users.email, credentials.email as string));
+
+        // Check if user exists
+        if (!user) {
+          throw new Error('Incorrect credentials');
+        }
+
+        // Compare the provided password with the stored hashed password
+        const passwordCorrect = await compare(
+          credentials.password as string,
+          user.password!,
+        );
+        if (!passwordCorrect) {
+          throw new Error('Incorrect credentials');
+        }
+
+        // Return the user object
+        return {
+          id: user.id.toString(),
+          email: user.email,
+        };
+      },
+    }),
+  ],
+});
