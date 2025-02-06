e56bd6ab4bb447bebe5872b7b819026ed5769463
Leo <37211774+CaliforniaDev@users.noreply.github.com>
2025-02-06 00:59:20 -0800

    feat: add password reset tokens schema and update main schema export

--- auth-app/db/schema.ts
+++ auth-app/db/schema.ts
@@ -1,1 +1,6 @@
+// To push the schema to the database, run the following command in the terminal:
+//*: npx drizzle-kit push 
+
+
 export { users } from './usersSchema';
+export { passwordTokens } from './passwordResetTokensSchema';
