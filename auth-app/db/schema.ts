7a4a614ec67a179cbce2b27eaae2953eb6e58a79
Leo <37211774+CaliforniaDev@users.noreply.github.com>
2025-02-06 02:33:54 -0800

    fix: update export for password reset tokens schema to use correct naming

--- auth-app/db/schema.ts
+++ auth-app/db/schema.ts
@@ -1,6 +1,7 @@
+import { passwordResetTokensSchema } from './passwordResetTokensSchema';
 // To push the schema to the database, run the following command in the terminal:
 //*: npx drizzle-kit push 
 
 
 export { users } from './usersSchema';
-export { passwordTokens } from './passwordResetTokensSchema';
+export { passwordResetTokensSchema } from './passwordResetTokensSchema';
