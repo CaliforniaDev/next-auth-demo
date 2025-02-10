51322e26608c40d7d118ae97c7411a52653ba2d2
Leo <37211774+CaliforniaDev@users.noreply.github.com>
2025-02-10 05:02:58 -0800

    feat: add Two Factor Authentication form to My Account page

--- auth-app/app/(logged-in)/my-account/page.tsx
+++ auth-app/app/(logged-in)/my-account/page.tsx
@@ -1,10 +1,13 @@
 import { auth } from '@/auth';
-import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
+import TwoFactorAuthForm from './two-factor-auth-form';
+
 import { Label } from '@/components/ui/label';
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
+
+
 
 export default async function MyAccount() {
   const session = await auth();
-
   return (
     <Card className='w-[350px]'>
       <CardHeader>
@@ -13,6 +16,7 @@ export default async function MyAccount() {
       <CardContent>
         <Label className='font-semibold'>Email Address</Label>
         <div className='text-muted-foreground'>{session?.user?.email}</div>
+        <TwoFactorAuthForm />
       </CardContent>
     </Card>
   );
---
+++ auth-app/app/(logged-in)/my-account/two-factor-auth-form/index.tsx
@@ -0,0 +1,9 @@
+'use client';
+
+export default function TwoFactorAuthForm() {
+  return (
+    <div>
+      <h1>Two Factor Auth Form</h1>
+    </div>
+  );
+}
